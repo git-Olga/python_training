d87200e5d4665275b44d73057d3389056d3961b6
git-Olga <olga.korobkova.post@gmail.com>
2019-09-30 20:13:09 +0300

    Выделены вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
-
+import unittest
+from group import Group
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
@@ -16,58 +11,60 @@ class UntitledTestCase(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_untitled_test_case(self):
+    def test_add_group(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
-        driver.find_element_by_link_text("groups").click()
+        self.open_home_page()
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="erert", header="ertertetret", footer="dsfsfsd"))
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_home_page()
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="", header="", footer=""))
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, driver):
+        driver.find_element_by_link_text("group page").click()
+
+    def create_group(self, driver, group):
+        # init group creation
         driver.find_element_by_name("new").click()
+        # fill group form
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("erert")
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("ertertetret")
+        driver.find_element_by_name("group_header").send_keys(group.header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("dsfsfsd")
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
         driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
-        driver.find_element_by_link_text("Logout").click()
+
+    def open_groups_page(self, driver):
+        driver.find_element_by_link_text("groups").click()
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
+    def open_home_page(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.driver.quit()
