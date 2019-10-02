8b02880c301ba351ee0b56e7df6b0301b0ebb4d6
git-Olga <olga.korobkova.post@gmail.com>
2019-10-02 21:44:12 +0300

    Сравнение списков групп при добавлении группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,9 +3,15 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="erert", header="ertertetret", footer="dsfsfsd"))
+    new_groups = app.group.get_group_list()
+    assert len(new_groups) - len(old_groups) == 1
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(new_groups) - len(old_groups) == 1
 
