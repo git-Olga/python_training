91094170883023dab497de1ec54f477d1212c286
git-Olga <olga.korobkova.post@gmail.com>
2019-10-03 20:50:35 +0300

    Добавлено хеширование для теста добавления группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
+
 def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="erert", header="ertertetret", footer="dsfsfsd")
     app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(new_groups) - len(old_groups) == 1
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(new_groups) - len(old_groups) == 1
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
+#    new_groups = app.group.get_group_list()
+#    assert len(new_groups) - len(old_groups) == 1
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
